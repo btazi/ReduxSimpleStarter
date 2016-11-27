c9cf27b6f009877956a3cdb39fd9da9bfb64b662
Badr Tazi <tasibadr@gmail.com>
2016-11-27 08:43:47 +0000

    end champter 1

---
+++ src/components/search_bar.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+
+class SearchBar extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			term: ''
+		};
+	}
+
+	render(){
+		return (
+			<div>
+				<input 
+					value={this.state.term}	
+					onChange={event => this.onInputChange(event.target.value)}
+					className="search-bar"
+				/>
+			</div>
+		)
+	}
+
+	onInputChange(term){
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
+};
+
+export default SearchBar;
